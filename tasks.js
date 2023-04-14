360860c1fdeabce98651e9385615f027fb29342a
Geisian dos Santos Reis <geisian.reis@levva.io>
2023-04-14 10:56:38 -0300

    JS - criando arquivo js e primairas tasks

---
+++ tasks.js
@@ -0,0 +1,13 @@
+let tasks = [
+    {
+        id: 1,
+        nome: 'Dar banho no cachorro',
+        toDo: false
+    },
+    {
+        id: 2,
+        nome: 'Visto na esquina',
+        toDo: true
+    }
+
+]
