4228d1b39df602910f5983371fb5149e8094697c
Geisian dos Santos Reis <geisian.reis@levva.io>
2023-04-17 09:55:28 -0300

    JS - implementação da função create new task element

--- tasks.js
+++ tasks.js
@@ -21,6 +21,51 @@ const addTasksInput = document.getElementById("tasks_input");
 const addTasksButton = document.getElementsByClassName("button")[0];
 const tasksList = document.getElementById("tasks_list");
 
+
+function createNewTaskElement(taskName, taskId) {
+
+    let task = document.createElement("li");
+    task.classList.add("task");
+    task.classList.add("todo");
+    task.setAttribute("id", taskId);
+
+    let leftContent = document.createElement("div");
+    leftContent.classList.add("left_content")
+
+    let todoIcon = document.createElement("i");
+    todoIcon.classList.add("ph-duotone");
+    todoIcon.classList.add("ph-circle-dashed");
+    todoIcon.classList.add("check_btn");
+    todoIcon.addEventListener("click", completeTask);
+
+    let doneIcon = document.createElement("i");
+    doneIcon.classList.add("ph-duotone");
+    doneIcon.classList.add("ph-check-circle");
+    doneIcon.classList.add("check_btn");
+    doneIcon.classList.add("hidden");
+    doneIcon.addEventListener("click", incompleteTask);
+
+    let name = document.createElement("p");
+    name.innerHTML = taskName;
+
+    let deleteIcon = document.createElement("i");
+    deleteIcon.classList.add("ph-duotone");
+    deleteIcon.classList.add("ph-trash");
+    deleteIcon.classList.add("delete_btn");
+    deleteIcon.addEventListener("click", deleteTask);
+
+    leftContent.appendChild(todoIcon);
+    leftContent.appendChild(doneIcon);
+    leftContent.appendChild(name);
+
+    task.appendChild(leftContent);
+    task.appendChild(deleteIcon);
+
+    return task;
+
+
+}
+
 function addTask(event) {
     event.preventDefault();
     console.log("Add Task");
